31bd3aad84b38faa2e86a350459e237727b39394
Youssef Muhiar <youssefmuhiar@gmail.com>
2025-09-03 18:36:44 -0500

    Delete product-data.js

--- product-data.js
+++
@@ -1,6 +0,0 @@
-const products = [
-{ name: "Classic Tee", price: 20, image: "https://source.unsplash.com/400x400/?tshirt" },
-{ name: "Denim Jeans", price: 45, image: "https://source.unsplash.com/400x400/?jeans" },
-{ name: "Leather Jacket", price: 120, image: "https://source.unsplash.com/400x400/?jacket" },
-{ name: "Sneakers", price: 60, image: "https://source.unsplash.com/400x400/?sneakers" }
-];
